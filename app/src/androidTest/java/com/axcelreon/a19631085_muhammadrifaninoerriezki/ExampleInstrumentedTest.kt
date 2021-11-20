8dab26eb27a78a73c89ae52a99cb6a5acff9b0c2
Muhammad Rifani Noer Riezki <rifaniriezki05@gmail.com>
2021-11-20 15:12:35 +0700

    intent,Splash

---
+++ app/src/androidTest/java/com/axcelreon/a19631085_muhammadrifaninoerriezki/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.axcelreon.a19631085_muhammadrifaninoerriezki
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.axcelreon.a19631085_muhammadrifaninoerriezki", appContext.packageName)
+    }
+}
